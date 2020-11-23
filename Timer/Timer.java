6cf81a49939e36b5d7b8f1953f9926c9197e50da
causeyj <72839136+causeyj@users.noreply.github.com>
2020-11-22 20:43:39 -0600

    Add files via upload

--- Timer/Timer.java
+++ Timer/Timer.java
@@ -1,6 +1,22 @@
+package Timer;
+
 public class Timer
 {
+    private java.util.Timer timer;
+
     Timer()
+    {
+        timer = new java.util.Timer();
+        timer.schedule(generateReport(), );
+        timer.schedule(clearAppointments(), );
+    }
+
+    private void generateReport()
+    {
+
+    }
+
+    private void clearAppointments()
     {
 
     }
