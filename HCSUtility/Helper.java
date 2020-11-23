3412aebea7a2705b736fd18cf32cfc235df2e0d2
causeyj <72839136+causeyj@users.noreply.github.com>
2020-11-22 20:43:07 -0600

    Add files via upload

---
+++ HCSUtility/Helper.java
@@ -0,0 +1,46 @@
+package HCSUtility;
+
+import GUI.ErrorScreen;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+final public class Helper
+{
+    private Helper()
+    {
+    }
+
+    static public ArrayList<String> loadChartData(String filePath)
+    {
+        try
+        {
+            File file = new File(filePath);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            ArrayList<String> chart = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                chart.add(line);
+            }
+
+            if (!Files.deleteIfExists(file.toPath()))
+            {
+                ErrorScreen err = new ErrorScreen("Could not delete patient_id.txt");
+                err.setVisible(true);
+            }
+
+            return chart;
+        }
+        catch (IOException e)
+        {
+            ErrorScreen err = new ErrorScreen(e.toString());
+            err.setVisible(true);
+            return new ArrayList<String>();
+        }
+    }
+}
