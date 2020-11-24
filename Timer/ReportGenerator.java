39421028b9fd8f32b2a6414643fb3cc78b3e7c80
JStrick510 <jacobstrickland510@gmail.com>
2020-11-24 10:20:18 -0600

    Timer for 9pm should work, no report generation yet

---
+++ Timer/ReportGenerator.java
@@ -0,0 +1,13 @@
+package project1;
+
+import java.util.*;
+
+public class ReportGenerator extends TimerTask{
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
---
+++ Timer/ReportManager.java
@@ -0,0 +1,33 @@
+package project1;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.time.LocalTime;
+
+public class ReportManager {
+	
+	public void generateReport()
+	{
+		//create a new instance of timer class
+		Timer timer = new Timer();
+		
+		//create an instance of task to be scheduled
+		TimerTask task = new ReportGenerator();
+		
+		int dayMS = 86400000; //24hrs in ms
+		int nineMS = 75600000; //time from 12AM to 9PM in ms
+		int currentTimeMS = LocalTime.now().getHour() * 3600000 + LocalTime.now().getMinute() * 60000 + LocalTime.now().getSecond() * 1000;
+		int delay = 0;
+		
+		if(currentTimeMS < nineMS)
+			delay = nineMS - currentTimeMS;
+		else
+			delay = 14400000 - currentTimeMS + nineMS; //four hours left in day after minus the current time which is between 9PM and 12AM plus the time from 12AM to 9PM
+		
+		//scheduling the timer instance
+		timer.schedule(task, delay, dayMS); 
+		
+		//timer.cancel(); //this is needed at when the program closes to close the timer
+	}
+
+}
